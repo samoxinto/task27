3fa51865d14d96da969930bc0cb43cc7d58701e8
Денис Степаненко <denis.stepanenko@hotmail.com>
2022-08-18 13:44:24 +0300

    Add project files.

---
+++ Task27/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            string number = Console.ReadLine();
+
+            int result = 0;
+            for (int i = 0; i < number.Length; i++)
+                result += int.Parse(number[i].ToString());
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
